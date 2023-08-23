d1563e8dc37774061c7934be1b03fd5e297775c4
s0urcemaster <sourcemaster@digi-craft.de>
2023-08-23 20:00:08 +0200

    switchWP

--- apps/digicraft3/app/worktime/components/Worktime.tsx
+++ apps/digicraft3/app/worktime/components/Worktime.tsx
@@ -7,7 +7,7 @@ import {daysHoursString, getFreeTime} from './lib'
 import {toLocale} from '../../../lib/math'
 import {workingDaysEffective, workingHours} from './year2023'
 import {useLocalStorage} from '../../LocalStorageContext'
-import { Button } from '@blueprintjs/core'
+import { Button, HTMLSelect, Icon } from '@blueprintjs/core'
 import moment from 'moment'
 
 function OverviewItem(props: {heading: string, rows: [string, number|string][]}) {
@@ -22,6 +22,10 @@ function OverviewItem(props: {heading: string, rows: [string, number|string][]})
 			</div>
 		</div>
 	)
+}
+
+function PrevButton() {
+
 }
 
 export default function Worktime() {
@@ -43,8 +47,8 @@ export default function Worktime() {
 	}
 
 	return (
-		<div style={{display: 'flex', columnGap: 10}}>
-			<div style={{width: calendarWidth}}>
+		<div style={{display: 'grid', gridTemplateColumns: 'auto 1fr', columnGap: 10}}>
+			<div style={{maxWidth: calendarWidth, overflowX: 'scroll'}}>
 				<Calendar />
 				{/*<ModalDialog title={'Berechnung für 2023, Baden Württemberg'} buttonTitle={'OK'} width={400} height={500}*/}
 				{/*				 visible={modalVisible}*/}
@@ -57,35 +61,90 @@ export default function Worktime() {
 				{/*</ModalDialog>*/}
 			</div>
 			<div>
-				<div style={{display: 'grid', gridTemplateColumns: '3fr 1fr 1fr 1fr', gap: 10}}>
-					<div>
-						<Button text={getDate()} style={{padding: '10px 20px 10px 20px', fontSize: 30, width: '100%'}} />
-						<div style={{display: 'flex'}}>
-							<Button text={moment(day.date).subtract(1, 'days').format('DD.MM.YYYY')} icon={'arrow-left'} />
-							<Button text={moment(day.date).add(1, 'days').format('DD.MM.YYYY')} rightIcon={'arrow-right'} />
-						</div>
-
+				<div style={{minWidth: 400}}>
+					<Button text={getDate()} style={{padding: '10px 20px 10px 20px', fontSize: 30, width: '100%'}} />
+					<div style={{display: 'grid', gridTemplateColumns: 'auto auto'}}>
+						<Button text={moment(day.date).subtract(1, 'days').format('DD.MM.YYYY')} icon={'arrow-left'} alignText={'left'} />
+						<Button text={moment(day.date).add(1, 'days').format('DD.MM.YYYY')} rightIcon={'arrow-right'} alignText={'right'} />
+						<Button text={'Juli'} icon={'arrow-left'} alignText={'left'} />
+						<Button text={'September'} rightIcon={'arrow-right'} alignText={'right'} />
+						<Button text={year -1} icon={'arrow-left'} alignText={'left'} />
+						<Button text={year +1} rightIcon={'arrow-right'} alignText={'right'} />
 					</div>
-					<h4 className={'bp5-heading'}>Soll</h4>
-					<h4 className={'bp5-heading'}>Ist</h4>
-					<h4 className={'bp5-heading'}>Diff</h4>
-					<h4 className={'bp5-heading'}>Woche</h4>
-					<div></div>
-					<div></div>
-					<div></div>
-					<h4 className={'bp5-heading'}>Monat {getMonth()}</h4>
-					<div></div>
-					<div></div>
-					<div></div>
-					<h4 className={'bp5-heading'}>Jahr {year}</h4>
-					<div></div>
-					<div></div>
-					<div></div>
-				</div>
-				<div style={{display: 'flex', columnGap: 10}}>
-					<Button text={year -1} icon={'arrow-left'} />
-					{/*<Button text={year} />*/}
-					<Button text={year +1} rightIcon={'arrow-right'} />
+					<div style={{display: 'grid', gridTemplateColumns: 'auto auto auto'}}>
+						<Button icon={'play'} />
+						<Button icon={'pause'} />
+						<Button icon={'stop'} />
+					</div>
+					<div style={{display: 'grid', gridTemplateColumns: '30px auto auto 28px auto auto 55px'}}>
+						<Button icon={'delete'} />
+						<HTMLSelect>
+							<option value="00">00</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+						</HTMLSelect>
+						<HTMLSelect>
+							<option value="00">00</option>
+							<option value="15">15</option>
+							<option value="30">30</option>
+							<option value="45">45</option>
+						</HTMLSelect>
+						<Button icon={'small-minus'} />
+						<HTMLSelect>
+							<option value="00">00</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+							<option value="04">04</option>
+							<option value="01">01</option>
+							<option value="02">02</option>
+							<option value="03">03</option>
+						</HTMLSelect>
+						<HTMLSelect>
+							<option value="00">00</option>
+							<option value="15">15</option>
+							<option value="30">30</option>
+							<option value="45">45</option>
+						</HTMLSelect>
+						<Button text={'12:59'} />
+					</div>
 				</div>
 				<OverviewItem heading={'Jahresarbeitszeit'} rows={[['Tage:', workingDaysEffective], ['Stunden:', workingHours]]} />
 				<OverviewItem heading={'Geleistet'} rows={[['Tage:', daysHoursString(hoursWorked, 1)], ['Stunden:', toLocale(hoursWorked, 1)]]} />
